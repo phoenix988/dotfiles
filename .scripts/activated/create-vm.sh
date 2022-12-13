cf8dff7bc462944a2574c78d549eba32b4e8088b
Karl Fredin <karlfredin@gmail.com>
2022-12-13 13:41:31 +0200

    updating scripts and dmenu scripts

---
+++ .scripts/activated/create-vm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VM_NAME="ubuntu01"
+MEM_SIZE="1024"
+VCPUS="4"
+ISO_FILE='http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/'
+DISK_SIZE="20"
+
+
+
+sudo virt-install \
+     --name ${VM_NAME} \
+     --memory=${MEM_SIZE} \
+     --vcpus=${VCPUS} \
+     --location ${ISO_FILE} \
+     --disk size=${DISK_SIZE}  \
+     --network network=eth-nat \
+     --graphics=none \
+     --unattended \
+      user-password-file="/home/karl/.password"
+     #--console pty,target_type=serial \
+     #--initrd-inject /home/karl/Documents/kickstart/pressed.cfg --extra-args="inst.ks=file:/pressed.cfg console=tty0 console=ttyS0,115200n8"
+     #--extra-args="ks=http://192.168.122.1/ks.cfg console=tty0 console=ttyS0,115200n8"
---
+++ .scripts/backup-scripts/save-files-backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+date_for_backup=$(date +%d-%h-%Y-%H-%M )  
+
+declare -A games_backup
+
+games_backup[cyberpunk2077]="/home/karl/.steam/debian-installation/steamapps/compatdata/4124252510/pfx/drive_c/users/steamuser/"
+
+game_name=$(printf '%s\n' "${!games_backup[@]}")
+
+for g_name in $(printf '%s\n' "${game_name[@]}") ; do
+
+
+game_path=$(printf '%s\n' "${games_backup[${g_name}]}")
+
+
+[ -d $HOME/Yandex.Disk/saved-games/$g_name ] || mkdir -p $HOME/Yandex.Disk/saved-games/$g_name
+
+
+tar --use-compress-program="pigz -7" -g $HOME/Yandex.Disk/saved-games/$g_name/$g_name.snar \
+    -c $game_path -f $HOME/Yandex.Disk/saved-games/$g_name/$g_name-$date_for_backup.tgz
+
+
+done
