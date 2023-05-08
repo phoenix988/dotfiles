11732608cf45af1b5690d2b26df8a2a140190604
phoenix988 <karlfredin@gmail.com>
2023-05-08 13:23:34 +0300

    updating my awesome config

---
+++ .config/awesome/autostart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#run "megasync"
+run "xscreensaver -no-splash"
+#run "/usr/bin/dropbox"
+#run "insync start"
+run "picom -b --config $HOME/.config/picom/picom.conf"
+run "nm-applet"
+run "mullvad-vpn"
+run "emacs --daemon"
+run "~/.fehbg"
+run "conky -c /home/karl/.config/conky/main"
+run "steam -silent -no-browser" 
+run "xfce4-power-manager"
+run "emacs --daemon"
+#run "lxsession"
