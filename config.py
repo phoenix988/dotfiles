8dd580868e30d318548338484376c2d2970a28f9
phoenix988 <karlfredin@gmail.com>
2023-05-10 12:25:55 +0300

    updating workspaces

--- config.py
+++ config.py
@@ -506,12 +506,12 @@ name="Action: "),
 
 group_names = [("", {'layout': 'bsp' ,'matches':[Match(wm_class=["Brave-browser-nightly", "Chromium" , "librewolf"])]}),
                ("", {'layout': 'bsp','matches':[Match(wm_class=["neo"])]}),
-               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["TeamViewer"])]}),
+               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["TeamViewer"])]}),
                ("", {'layout': 'max', 'matches':[Match(wm_class=["lutris" , "Steam" , "upc.exe" , "steam_proton" , "heroic"])]}),
-               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["re.sonny.Tangram", "crx_cifhbcnohmdccbgoicgdjpfamggdegmo", "disk.yandex.com__client_disk"])]}),
+               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["re.sonny.Tangram", "crx_cifhbcnohmdccbgoicgdjpfamggdegmo", "disk.yandex.com__client_disk"])]}),
                ("", {'layout': 'max', 'matches':[Match(wm_class=["discord" , "Franz" , "whatsapp-nativefier-d40211" , "altus" , "whatsdesk" , "whatsapp-for-linux", "web.whatsapp.com"])]}),
-               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["Spotify"])]}),
-               ("", {'layout': 'treetab', 'matches':[Match(wm_class=["io.github.celluloid_player.Celluloid" , "urxvt" , "obs", "youtube.com", "netflix.com"])]}),
+               ("", {'layout': 'bsp', 'matches':[Match(wm_class=["Spotify"])]}),
+               ("", {'layout': 'treetab', 'matches':[Match(wm_class=["io.github.celluloid_player.Celluloid" , "urxvt" , "obs", "youtube.com", "netflix.com"])]}),
                ("", {'layout': 'bsp', 'matches':[Match(wm_class=["gimp-2.10","Gimp" ,"Cinelerra","Olive", "kdenlive" , "resolve" ])]})]
 
 
@@ -806,7 +806,7 @@ def init_widgets_list():
                        fontsize = 45
                        ),
              widget.TextBox(
-                       text = '★',
+                       text = '' ,
                        background = colors[0],
                        foreground = colors[10],
                        padding = -1,
