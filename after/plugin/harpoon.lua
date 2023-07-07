e16392d7547a4cca99e52a364a35c136e6a17195
phoenix988 <karlfredin@gmail.com>
2023-07-07 17:27:51 +0300

    NEW: adding alpha dashboard plugin

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require("harpoon.mark")
 local ui   = require("harpoon.ui")
 
 function to_harpoon(arg)
-   ui.nav_file(arg) 
+   ui.nav_file(arg)
 end
 
 vim.cmd("command! -nargs=1 ToHarpoon lua to_harpoon(tonumber(<f-args>))")
