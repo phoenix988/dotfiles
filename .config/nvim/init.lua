c5bba18dd2b05eb6926dcdb4d907650e83db11ac
Karl <77921236+phoenix988@users.noreply.github.com>
2023-12-28 15:17:52 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,7 @@
 -- set space as leader
---vim.g.mapleader = ' '
---vim.g.maplocalleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-require("lugitive.status")
 require("karl.lazy")
 require("karl.remap")
 require("karl.set")
